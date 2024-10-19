987752e8387a7242224de31add910fe43522e849
Andrey Baryshev <andrey.bar66@gmail.com>
2024-10-19 22:44:44 +0500

    Add implementation JDAv5

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0")) // Тесты
     testImplementation("org.junit.jupiter:junit-jupiter") // Тесты
     implementation("org.reflections:reflections:0.10.2") // Reflections (Для получения классов с пакетов)
-    //implementation("net.dv8tion:JDA:5.1.0") // Библиотека для работы с Discord
+    implementation("net.dv8tion:JDA:5.1.0") // Библиотека для работы с Discord
     implementation("org.slf4j:slf4j-nop:2.0.7") // Логирование (Зависимость Reflection)
     //implementation("org.springframework.boot:spring-boot-starter-data-redis") // Кэширование
     //implementation("org.springframework.boot:spring-boot-starter-data-cassandra") // База данных
