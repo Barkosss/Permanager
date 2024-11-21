612587e4fe07acd28f7c034313ab5c8068a0c91f
Andrey Baryshev <andrey.bar66@gmail.com>
2024-11-21 21:39:18 +0500

    Add database

--- src/main/java/common/utils/DatabaseHandler.java
+++ src/main/java/common/utils/DatabaseHandler.java
@@ -1,10 +1,40 @@
 package common.utils;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
 // TODO: Подумать о реализации с Redis
 /**
  * Обработчик запросов в Базу Данных
  */
 public class DatabaseHandler {
+
+    public static Connection getConnection() throws SQLException {
+        String url = "jdbc:postgresql://localhost:8080/mydatabase";  // Адрес базы данных
+        String user = "barkos";  // Ваше имя пользователя
+        String password = "1234";  // Ваш пароль
+
+        return DriverManager.getConnection(url, user, password);
+    }
+
+    public static void main(String[] args) {
+        try (Connection conn = getConnection()) {
+            System.out.println("Connect accepted");
+            // Выполнение SQL-запросов...
+        } catch (SQLException err) {
+            try (FileWriter writer = new FileWriter("output.txt")) {
+                writer.write(err.getMessage()); // Запись строки в файл
+                System.out.println("File is writer");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Error connection");
+        }
+    }
 
     /**
      * Запись значения(-й)
