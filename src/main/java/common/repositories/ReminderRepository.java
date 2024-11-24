aae05316b1d811c459bf9332766b202e97d42d94
Andrey Baryshev <andrey.bar66@gmail.com>
2024-11-24 13:34:27 +0500

    Create ReminderHandler & ReminderRepository & Other

---
+++ src/main/java/common/repositories/ReminderRepository.java
@@ -0,0 +1,40 @@
+package common.repositories;
+
+import common.models.Reminder;
+import common.utils.LoggerHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ReminderRepository {
+    LoggerHandler logger = new LoggerHandler();
+    public Map<Long, Reminder> reminders;
+
+    public ReminderRepository() {
+        this.reminders = new HashMap<>();
+    }
+
+    // Создать напоминание в памяти
+    public void create(Reminder reminder) {
+        long reminderId = reminder.getId();
+        if (reminders.containsKey(reminderId)) {
+            return;
+        }
+        reminders.put(reminderId, reminder);
+    }
+
+    // Найти напоминание по ID у пользователя
+    public Reminder findById(long reminderId) {
+        Reminder reminder;
+        if ((reminder = reminders.get(reminderId)) != null) {
+            return reminder;
+        }
+        logger.error("Reminder by id(" + reminderId + ") is not found");
+        return null;
+    }
+
+    // Существует ли напоминание у пользователя
+    public boolean existsById(long reminderId) {
+        return reminders.get(reminderId) != null;
+    }
+}
