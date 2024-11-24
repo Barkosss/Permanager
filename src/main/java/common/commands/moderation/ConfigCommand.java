bab696464ab674ce245fb1aa1502378fb868ccd6
Andrey Baryshev <andrey.bar66@gmail.com>
2024-11-24 19:13:20 +0500

    Create config command

--- src/main/java/common/commands/moderation/ConfigCommand.java
+++ src/main/java/common/commands/moderation/ConfigCommand.java
@@ -1,9 +1,13 @@
 package common.commands.moderation;
 
 import common.commands.BaseCommand;
+import common.iostream.OutputHandler;
 import common.models.Interaction;
 
+import java.util.List;
+
 public class ConfigCommand implements BaseCommand {
+    OutputHandler output = new OutputHandler();
 
     @Override
     public String getCommandName() {
@@ -15,8 +19,49 @@ public class ConfigCommand implements BaseCommand {
         return "";
     }
 
+    private boolean parseArgs(Interaction interaction) {
+        List<String> arguments = interaction.getArguments();
+        String argument;
+        for (int index = 0; index < arguments.size(); index++) {
+            argument = arguments.get(index).toLowerCase();
+            switch (index) {
+                case 0: { // Проверяем первый аргумент
+                    switch (argument) {
+                        case "dashboard": {
+                            String message = "";
+                            output.output(interaction.setMessage(message));
+                            return true;
+                        }
+
+                        case "user": {
+                            // Проверить аргументы на настройку пользователя
+                            break;
+                        }
+
+                        case "group": {
+                            // Проверить аргументы на настройку групп
+                            break;
+                        }
+                    }
+                    break;
+                }
+
+                case 1: { // Проверяем второй аргумент
+
+                    break;
+                }
+            }
+        }
+
+        return false;
+    }
+
     @Override
     public void run(Interaction interaction) {
+        if (parseArgs(interaction)) {
+            return;
+        }
+
 
     }
 }
