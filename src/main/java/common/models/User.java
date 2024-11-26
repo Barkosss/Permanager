26f6a81dd8ca67246f6fee03d227228c891f5be8
ci7iZ4n <dyuha.gorodilov@ya.ru>
2024-11-26 14:25:31 +0500

    update

--- src/main/java/common/models/User.java
+++ src/main/java/common/models/User.java
@@ -2,60 +2,38 @@ package common.models;
 
 public class User {
 
-    public enum InputStatus {
-        WAITING,
-        COMPLETED
-    }
-
-    // Идентификатор пользователя
     long userId;
 
     // Объект с информацией ожидаемых ответов
-    InputExpectation userInputExpectation = new InputExpectation();
+    InputExpectation userInputExpectation;
 
-    // Состояние ввода
-    InputStatus inputStatus;
+    Interaction.Platform platform;
 
     public User(long userId) {
         this.userId = userId;
     }
 
-    public InputStatus getInputStatus() {
-        if (inputStatus == null) {
-            this.inputStatus = InputStatus.COMPLETED;
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public Interaction.Platform getPlatform() {
+        return platform;
+    }
+
+    public void setPlatform(Interaction.Platform platform) {
+        this.platform = platform;
+    }
+
+    public InputExpectation getUserInputExpectation() {
+        if (userInputExpectation == null) {
+            userInputExpectation = new InputExpectation();
         }
-        return inputStatus;
-    }
-
-    public void setValue(Object value) {
-        InputExpectation inputExpectation = this.userInputExpectation;
-        inputExpectation.getExpectedInputs().get(inputExpectation.expectedCommandName)
-                .put(inputExpectation.expectedInputKey, value);
-    }
-
-    public Object getValue(String commandName, String key) {
-        return this.userInputExpectation.getExpectedInputs().get(commandName).get(key);
-    }
-
-    public String getCommandException() {
-        return this.userInputExpectation.expectedCommandName;
-    }
-
-    public boolean isExceptedKey(String commandName, String key) {
-        if (this.userInputExpectation.getExpectedInputs().isEmpty()) {
-            return false;
-        }
-        return this.userInputExpectation.getExpectedInputs().get(commandName).containsKey(key);
-    }
-
-    public User setExcepted(String commandName, String valueKey) {
-        this.inputStatus = InputStatus.WAITING;
-        this.userInputExpectation.setExpected(commandName, valueKey);
-        return this;
-    }
-
-    public void clearExpected(String commandName) {
-        this.inputStatus = InputStatus.COMPLETED;
-        this.userInputExpectation.getExpectedInputs().remove(commandName);
+        return userInputExpectation;
     }
 }
