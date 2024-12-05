60c0d9fa0d376c139b72ad12a35e01cd72fbcc58
Barkosss <andrey.bar66@gmail.com>
2024-12-05 11:53:27 +0500

    Code style fix

--- src/main/java/common/commands/custom/TaskCommand.java
+++ src/main/java/common/commands/custom/TaskCommand.java
@@ -72,8 +72,6 @@ public class TaskCommand implements BaseCommand {
                 }
                 break;
             }
-            case "edit": {
-            }
             case "remove": {
                 user.setExcepted(getCommandName(), "action").setValue(firstArg);
                 if (arguments.isEmpty()) {
