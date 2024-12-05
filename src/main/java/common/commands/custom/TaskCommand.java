5e4f76d43624fb53578fe2e0331bf8be57e7ddf7
Andrey Baryshev <77407266+Barkosss@users.noreply.github.com>
2024-12-05 11:45:33 +0500

    Dev barkos (#104)
    * update (#102)
    
    Co-authored-by: ci7iZ4n <dyuha.gorodilov@ya.ru>
    
    * Hotfix
    
    ---------
    
    Co-authored-by: ci7iZ4n <dyuha.gorodilov@ya.ru>

--- src/main/java/common/commands/custom/TaskCommand.java
+++ src/main/java/common/commands/custom/TaskCommand.java
@@ -203,7 +203,9 @@ public class TaskCommand implements BaseCommand {
         long taskId = (long) user.getValue(getCommandName(), "taskIndex");
         Task editedTask = user.getTasks(interaction.getChatId()).get(taskId);
         user.getTasks(interaction.getChatId()).remove(taskId);
+    }
 
-
+    public void list(Interaction interaction, User user) {
+        return;
     }
 }
