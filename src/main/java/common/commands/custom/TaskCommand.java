46c810fed2ddecdf44fb4f56e05ff4640dfb3c47
Barkosss <andrey.bar66@gmail.com>
2024-12-05 11:49:11 +0500

    Code style fix

--- src/main/java/common/commands/custom/TaskCommand.java
+++ src/main/java/common/commands/custom/TaskCommand.java
@@ -49,7 +49,8 @@ public class TaskCommand implements BaseCommand {
             case "edit": {
                 user.setExcepted(getCommandName(), "action").setValue(firstArg);
                 if (arguments.size() >= 2) {
-                    Optional<LocalDate> date = validate.isValidDate(arguments.get(arguments.size() - 2) + " " + arguments.getLast());
+                    Optional<LocalDate> date = validate.isValidDate(arguments.get(arguments.size() - 2)
+                            + " " + arguments.getLast());
                     Optional<LocalDate> time = validate.isValidTime(arguments.getLast());
                     arguments.subList(0, arguments.size() - 2);
                 } else if (!arguments.isEmpty()) {
@@ -163,9 +164,9 @@ public class TaskCommand implements BaseCommand {
         user.clearExpected(getCommandName());
     }
 
-    public void remove(Interaction interaction, User user){
+    public void remove(Interaction interaction, User user) {
         logger.debug("Remove task is start");
-        if (!user.isExceptedKey(getCommandName(), "taskIndex")){
+        if (!user.isExceptedKey(getCommandName(), "taskIndex")) {
             user.setExcepted(getCommandName(), "taskIndex");
             output.output(interaction.setMessage("Enter the taskIndex:"));
             logger.debug("Task command requested a taskIndex");
@@ -176,7 +177,7 @@ public class TaskCommand implements BaseCommand {
 
     public void edit(Interaction interaction, User user) {
         logger.debug("Remove task is start");
-        if (!user.isExceptedKey(getCommandName(), "taskIndex")){
+        if (!user.isExceptedKey(getCommandName(), "taskIndex")) {
             user.setExcepted(getCommandName(), "taskIndex");
             output.output(interaction.setMessage("Enter the taskIndex:"));
             logger.debug("Task command requested a taskIndex");
