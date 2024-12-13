85c486caf3ee9bea7f73a838bf781429a36b1d95
Andrey Baryshev <77407266+Barkosss@users.noreply.github.com>
2024-12-13 16:40:29 +0500

    Update WarnsCommand.java

--- src/main/java/common/commands/custom/WarnsCommand.java
+++ src/main/java/common/commands/custom/WarnsCommand.java
@@ -6,12 +6,14 @@ import common.models.Interaction;
 import common.models.InteractionTelegram;
 import common.models.User;
 import common.utils.LoggerHandler;
+import common.utils.Validate;
 
 import java.util.List;
 
 public class WarnsCommand implements BaseCommand {
     OutputHandler output = new OutputHandler();
     LoggerHandler logger = new LoggerHandler();
+    Validate validate = new Validate();
 
     @Override
     public String getCommandName() {
@@ -34,7 +36,18 @@ public class WarnsCommand implements BaseCommand {
             return;
         }
 
-        // Если аргументы не пустые - смотрим у кого-то
+        // Если аргумент не пустой - Смотрим по Id первым аргументом
+        Optional<Integer> validInteger = validate.isValidInteger(arguments.getFirst());
+        if (validInteger.isPreset()) {
+            ChatMember chatMember = interactionTelegram.telegramBot.execute(new GetChatMember(interaction.getChatId(), arguments.getFirst()));
+            // Если такой пользователь есть в чате
+            if (chatMember != null) {
+                user.setExcepted(getCommandName(), "userId").setValue(chatMember.user().id());
+                return;
+            }
+        }
+
+        // Если есть ответное сообщение - смотрим у кого-то (И первый аргумент пустой или некорректный)
         if (interactionTelegram.getContentReply() != null) {
             long userId = interactionTelegram.getContentReply().from().id();
             user.setExcepted(getCommandName(), "userId").setValue(userId);
@@ -55,13 +68,14 @@ public class WarnsCommand implements BaseCommand {
         long targetUserId = (long) user.getValue(getCommandName(), "userId");
 
         try {
-            output.output(interaction.setLanguageValue("", List.of(String.valueOf(targetUserId))));
+            output.output(interaction.setLanguageValue("warns.complete", List.of(String.valueOf(targetUserId))));
             logger.info(String.format("User by id(%d) in chat by id(%d) has look warns at target user by id (%d)",
                     user.getUserId(), interaction.getChatId(), targetUserId));
-            user.clearExpected(getCommandName());
         } catch (Exception err) {
             output.output(interaction.setLanguageValue("system.error.something"));
             logger.error(String.format("Warns not show for user by id(%d)", user.getUserId()));
+        } finally {
+            user.clearExpected(getCommandName());
         }
     }
 }
