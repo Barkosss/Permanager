e507b042fd0860318139ad974f5456e09ad8ef15
Andrey Baryshev <andrey.bar66@gmail.com>
2025-02-12 22:56:19 +0500

    Update command clear

--- src/main/java/common/commands/moderation/ClearCommand.java
+++ src/main/java/common/commands/moderation/ClearCommand.java
@@ -2,7 +2,6 @@ package common.commands.moderation;
 
 import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.model.Update;
-import com.pengrad.telegrambot.request.DeleteMessage;
 import com.pengrad.telegrambot.request.DeleteMessages;
 import com.pengrad.telegrambot.request.GetChatMemberCount;
 import com.pengrad.telegrambot.request.GetUpdates;
@@ -90,72 +89,5 @@ public class ClearCommand implements BaseCommand {
         interactionTelegram.execute(new DeleteMessages(chatId, arrayMessagesId));
         output.output(interactionTelegram.setLanguageValue(""));
         user.clearExpected(getCommandName());
-    }
-
-    public void runOld(Interaction interaction) {
-
-        if (interaction.getPlatform() == Interaction.Platform.CONSOLE) {
-            output.output(interaction.setMessage("This command is not available for the console"));
-            return;
-        }
-
-        User user = interaction.getUser(interaction.getUserId());
-        InteractionTelegram interactionTelegram = ((InteractionTelegram) interaction);
-
-        if (!user.hasPermission(interaction.getChatId(), ModerationCommand.CLEAR)) {
-            output.output(interaction.setLanguageValue("system.error.accessDenied"));
-            return;
-        }
-
-        // Парсинг аргументов
-        parseArgs(interactionTelegram, user);
-
-        if (interactionTelegram.execute(new GetChatMemberCount(interaction.getChatId())).count() <= 2) {
-            output.output(interaction.setMessage("This command is not available for private chat"));
-            return;
-        }
-
-        // Получаем пользователя
-        if (interactionTelegram.getContentReply() != null) {
-            user.setExcepted(getCommandName(), "user").setValue(interactionTelegram.getContentReply());
-        }
-
-        int countMessages = 0;
-        List<Integer> messagesIds = new ArrayList<>(List.of());
-
-        // Получаем количество удаляемых сообщений
-        if (!user.isExceptedKey(getCommandName(), "countMessages")) {
-            user.setExcepted(getCommandName(), "countMessages", InputExpectation.UserInputType.INTEGER);
-            logger.info("Clear command requested a counter argument");
-            output.output(interactionTelegram.setMessage("Enter count messages for delete"));
-            return;
-        }
-
-        GetUpdates getUpdates = new GetUpdates().limit(100);
-        GetUpdatesResponse updatesResponse = interactionTelegram.execute(getUpdates);
-        List<Update> updates = updatesResponse.updates();
-
-        int maxCountMessages = (Integer) user.getValue(getCommandName(), "countMessages");
-        for (Update update : updates) {
-            Message message = update.message();
-
-            if (user.getValue(getCommandName(), "user") != null && message.from().id() == interaction.getUserId()) {
-                messagesIds.add(message.messageId());
-            } else if (user.getValue(getCommandName(), "user") == null) {
-                messagesIds.add(message.messageId());
-            }
-            countMessages++;
-
-            if (countMessages <= maxCountMessages) {
-                break;
-            }
-        }
-
-        int[] arrayMessagesIds = new int[messagesIds.size()];
-        for (int index = 0; index < messagesIds.size(); index++) {
-            arrayMessagesIds[index] = messagesIds.get(index);
-        }
-
-        interactionTelegram.execute(new DeleteMessages(interaction.getChatId(), arrayMessagesIds));
     }
 }
