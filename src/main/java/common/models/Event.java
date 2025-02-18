16af98b539499b05384cdbcdd9b0101ada3d06f6
Andrey Baryshev <77407266+Barkosss@users.noreply.github.com>
2025-02-18 20:39:02 +0500

    Revert "Update commands (Barkos) (#117)" (#119)
    This reverts commit c7fe3592cb5c4a9b4658ae1f0a075f82f7530d76.

--- src/main/java/common/models/Event.java
+++ src/main/java/common/models/Event.java
@@ -1,17 +1,5 @@
 package common.models;
 
 public class Event {
-
-    //название
-    String title;
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-
+    // ...
 }
