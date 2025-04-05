fe69482c5af642a5ede0a2717057bec1d3732676
Barkosss <andrey.bar66@gmail.com>
2025-04-05 14:02:27 +0500

    Add method "getCommands"

--- src/main/java/common/repositories/CommandRepository.java
+++ src/main/java/common/repositories/CommandRepository.java
@@ -1,17 +1,17 @@
 package common.repositories;
 
 import common.commands.BaseCommand;
-import common.utils.LoggerHandler;
 
 import java.util.Map;
 
 public class CommandRepository {
-    private final LoggerHandler logger = new LoggerHandler();
-    private final Map<String, BaseCommand> commands;
+    Map<String, BaseCommand> commands;
 
     public CommandRepository(Map<String, BaseCommand> commands) {
         this.commands = commands;
     }
+
+    public Map<String, BaseCommand> getCommands() { return commands; }
 
     public boolean hasCommand(String command) {
         return commands.containsKey(command);
