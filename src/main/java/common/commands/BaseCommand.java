a57c433abffea6b355fefb2cc6c269e310cf5338
Andrey Baryshev <andrey.bar66@gmail.com>
2025-04-09 22:14:50 +0500

    Edit comments

--- src/main/java/common/commands/BaseCommand.java
+++ src/main/java/common/commands/BaseCommand.java
@@ -22,6 +22,7 @@ public interface BaseCommand {
     /**
      * Получить описание команды
      *
+     * @param interaction Object Interaction
      * @return String
      */
     String getCommandDescription(Interaction interaction);
@@ -29,13 +30,15 @@ public interface BaseCommand {
     /**
      * Обработка аргументов, которые пользователь указал в сообщении
      *
-     * @param interaction Object interaction
-     * @param user        Object user
+     * @param interaction Object Interaction
+     * @param user        Object User
      */
     void parseArgs(Interaction interaction, User user);
 
     /**
      * Запустить команду
+     *
+     * @param interaction Object Interaction
      */
     void run(Interaction interaction);
 }
